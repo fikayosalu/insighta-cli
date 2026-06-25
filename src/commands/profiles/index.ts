a3e4f44ba583a1df2915cf023cd1bb0c9892f480
fikayo salu <salufikayo@gmail.com>
2026-06-25 23:11:38 +0100

    feat(profiles): set up command to create a profile

--- src/commands/profiles/index.ts
+++ src/commands/profiles/index.ts
@@ -1,8 +1,12 @@
 import { Command } from "commander";
 import { get } from "./subcommands/getAProfile";
 import { list } from "./subcommands/listProfiles";
+import { create } from "./subcommands/createProfile";
+import { search } from "./subcommands/searchProfile";
 
 export const profile = new Command("profile");
 
 profile.addCommand(list);
 profile.addCommand(get);
+profile.addCommand(create);
+profile.addCommand(search);
---
+++ src/commands/profiles/subcommands/createProfile.ts
@@ -0,0 +1,38 @@
+import { Command } from "commander";
+import { loadCredentials } from "../../../utils/credentials";
+import axios from "axios";
+import { API_URL } from "../../../config";
+import { Profile } from "../../../types";
+import Table from "cli-table3";
+import { axiosErrorHandler } from "../../../utils/helpers";
+
+export const create = new Command("create")
+	.description("Create a profile")
+	.option("--name <name>", "name of the profile to create")
+	.action(async (options) => {
+		const tokens = loadCredentials();
+		if (!tokens) return console.log("Please log in");
+
+		try {
+			const response = await axios.post(
+				`${API_URL}/api/profiles`,
+				{
+					name: options.name,
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${tokens.access_token}`,
+						"X-API-Version": 1,
+					},
+					timeout: 5000,
+					timeoutErrorMessage: "Request time out",
+				},
+			);
+			const profile: Profile = response.data.data;
+			const table = new Table({ head: Object.keys(profile) });
+			table.push(Object.values(profile));
+			console.log(table.toString());
+		} catch (error) {
+			axiosErrorHandler(error);
+		}
+	});
